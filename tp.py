38f181e1846999795ab78e121a57b54d4b1665b0
micaelavera <micavera2010@hotmail.com>
2022-09-06 16:29:12 -0300

    add matriz

--- tp.py
+++ tp.py
@@ -1,5 +1,6 @@
 from pyscipopt import Model, quicksum
 import numpy as np
+
 model = Model("tp")
 #file = input("Ingrese el nombre del archivo y su extensión. Por ejemplo: input1.txt\n")
 file = "input1.txt"
@@ -10,12 +11,7 @@ cant_necesaria = []
 remedios = []
 r_drogas = []
 cantidades = []
-
-
-
-#def llenar_matriz(remedios, r_drogas):
- #   matriz[0][1] = 2
-  #  return matriz
+tupla = [] 
 
 try:
     with open(file, 'r') as archivo:
@@ -33,9 +29,16 @@ try:
                     split = linea.split(" ")
                     remedios.append(int(split[0]))
                     r_drogas.append(int(split[1]))
+                    
+                    A = np.array([int(split[0]), int(split[1]), int(split[2])]) 
+                    tupla.append(A) 
 
-        matriz = np.zeros((max(remedios),len(r_drogas)-1)).astype(int)
-                                   
+                    
+        cantidades = np.zeros((max(remedios),len(r_drogas)-1)).astype(int)
+
+        for i in range(len(tupla)):
+            cantidades[(tupla[i][0])-1][(tupla[i][1])-1] = tupla[i][2] # m remedios (filas) y n drogas (columnas)
+
     archivo.close()   
 except IOError:
     print ("No existe el archivo", file)
@@ -56,25 +59,13 @@ for i in range(len(cant_necesaria)):
 # Remedios
 r = {}
 for i in range(len(remedios)):
-    r[i] = model.addVar(vtype='I')
+    r[i] = model.addVar(vtype='I', name='Remedio %d'%(i))
     r[i] = remedios[i]
 
-# Droga en el remedio i
-rd = {}
-for i in range(len(r_drogas)):
-    rd[i] = model.addVar(vtype='I')
-    rd[i] = r_drogas[i]
-
-# Cantidad de la droga en el remedio i
-# x = {}
-# for i in range(len(cantidades)):
-#     x[i] = model.addVar(vtype='I')
-#     x[i] = cantidades[i]
-
-
 #### RESTRICCIONES ####
-    #model.addCons(quicksum(d[j][i]*x[j] for j in F) == z[i], name="Nutr(%s)"%i)
-
+#for j in range(len(d)):
+ ##model.addCons(quicksum(cantidades[d[j]][r[t]] for t in range(len(r))) >= c[j-1])
+        
 #### FUNCIÓN OBJETIVO ####
 model.setObjective(quicksum(r[i] for i in r), "minimize")
 
@@ -82,5 +73,7 @@ model.hideOutput()
 model.optimize()
 
 print("\n******************************* SOLUCIÓN *******************************")
+print("Matriz de cantidades del remedio i y droga j")
+print(cantidades)
 
 print("Solución:", model.getObjVal()) 
